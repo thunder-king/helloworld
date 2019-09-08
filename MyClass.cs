1c732a7fd0cc31b6f582b2758cb43f120130547d
thunder-king <thunder.sheils@hotmail.com>
2019-09-08 12:54:13 +0200

    initial commit

---
+++ MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    public class MyClass
+    {
+        public string ReturnMessage()
+        {
+            return "Thunder is the coolest kid on the block! says his dad.";
+        }
+    }
+}
